b0e8f664d8c27aea8b4f11d413f291f7593642cb
Andrew Colombi <andrew.colombi@gmail.com>
2014-03-27 18:46:51 -0400

    maps work

--- BeerApp/src/com/example/beerapp/Location.java
+++ BeerApp/src/com/example/beerapp/Location.java
@@ -1,7 +1,10 @@
 package com.example.beerapp;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.maps.MapActivity;
 
 import android.os.Bundle;
@@ -25,18 +28,38 @@ public class Location extends Activity implements OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-        googleMap.getUiSettings().setMyLocationButtonEnabled(true);
-        googleMap.setMyLocationEnabled(true);
-       
+        
+        try {
+            // Loading map
+            initilizeMap();
+ 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+ 
     }
-    
+ 
+    /**
+     * function to load map. If map is not created it will create it for you
+     * */
     private void initilizeMap() {
+    	Log.i("map", "im0");
         if (googleMap == null) {
+        	Log.i("map", "im1");
             googleMap = ((MapFragment) getFragmentManager().findFragmentById(
                     R.id.map)).getMap();
- 
             googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+            googleMap.setMyLocationEnabled(true);
+            
+           
+            // check if map is created successfully or not
+            if (googleMap == null) {
+                Toast.makeText(getApplicationContext(),
+                        "Sorry! unable to create maps", Toast.LENGTH_SHORT)
+                        .show();
+            }
         }
+        Log.i("map", "im2");
     }
  
     @Override
